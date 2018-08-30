5cc3842fe3f71c196556ae2b4567f94295b7fd68
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-08-30 23:15:56 +0530

    Changes: 1.) Added Initial Version.

---
+++ src/com/example/MainClient.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.swing.JEditorPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.EditorKit;
+
+public class MainClient {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println(rtfToHtml(new FileReader(new File("Media_Outreach_Handbook_New TOC.rtf"))));
+
+    }
+
+    public static String rtfToHtml(Reader rtf) throws IOException {
+        JEditorPane p = new JEditorPane();
+        p.setContentType("text/rtf");
+        EditorKit kitRtf = p.getEditorKitForContentType("text/rtf");
+        try {
+            kitRtf.read(rtf, p.getDocument(), 0);
+            kitRtf = null;
+            EditorKit kitHtml = p.getEditorKitForContentType("text/html");
+            Writer writer = new StringWriter();
+            kitHtml.write(writer, p.getDocument(), 0, p.getDocument().getLength());
+            return writer.toString();
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
